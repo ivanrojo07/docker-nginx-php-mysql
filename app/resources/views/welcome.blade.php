b8d0de8c37dad028c221089276c5fc24957d5d47
Ivan Rojo <ivanrojo07@gmail.com>
2024-04-17 17:44:06 -0600

    Prueba socket

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hola Mundo a todos</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -38,7 +38,7 @@
                                         href="{{ route('login') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
-                                        Log in
+                                        Iniciar sesión
                                     </a>
 
                                     @if (Route::has('register'))
